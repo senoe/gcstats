fb5bb4ebb9e3ed0877dcb1dfb3c23e5c199c599f
senoe <senoe@mirxge.cc>
2021-03-30 10:11:57 -0700

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -92,7 +92,7 @@ module.exports = {
             `Last Online: §9${lastOnlineString}`,
             `Last Seen: §9${lastSeenString}`,
             ``,
-            `Known Aliases: §2${knownAliases[0].join(", ")}`,
+            //`Known Aliases: §2${knownAliases[0].join(", ")}`,
             player.socialMedia && player.socialMedia.links && player.socialMedia.links.HYPIXEL   ? `Forums: §6${player.socialMedia.links.HYPIXEL}` : null,
             player.socialMedia && player.socialMedia.links && player.socialMedia.links.DISCORD   ? `Discord: §6${player.socialMedia.links.DISCORD}` : null,
             player.socialMedia && player.socialMedia.links && player.socialMedia.links.TWITTER   ? `Twitter: §6${player.socialMedia.links.TWITTER}` : null,
